e64cfb75356ddc3aa448734fbc9aa0c6ebe9065a
dlobera <delobera@gmail.com>
2022-07-25 21:20:54 -0500

    add card page

---
+++ src/pages/AddCard/AddCard.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+
+function AddCard(props) {
+  return (
+    <>
+      <h1>Add a Card</h1>
+      <form autoComplete='off'>
+        <div>
+
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default AddCard
