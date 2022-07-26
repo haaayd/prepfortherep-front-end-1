7eacfa84964d73085019d6b93e348e714b51e44f
haayd <haydee.sramirez@gmail.com>
2022-07-26 15:44:18 -0700

    card

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
+
 const PostCard = ({card}) => {
   return (  
     <>
       <div className="card">
           <div className="card-body">
+            <Link to={"/cards"}>
             <h2>{card.title}</h2>
+            </Link>
 
           </div>
       </div>
